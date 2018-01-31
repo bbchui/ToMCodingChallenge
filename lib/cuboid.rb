dd15649665b8df586051930b37e64c83d5093e97
Brandon Chui <bbchui325@gmail.com>
2018-01-31 01:07:10 -0800

    initialize, vertices, move_to, intersect? + rspec

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,27 +1,55 @@
-
 class Cuboid
-  def initialize(x, y, z, length, width, height)
-    @origin = [x,y,z]
-    @length = length # x-axis
-    @width = width # z-axis
+  attr_accessor :origin, :leng, :width, :height
+
+  # Assuming cuboid cannot have a negative length, width, height
+  def initialize(origin, width, height, leng)
+    raise 'Length, width, and height must be greater than Zero' if [leng, width, height].any? {|num| num <= 0}
+
+    @origin = origin
+    @width = width # x-axis
     @height = height # y-axis
+    @leng = leng # z-axis
   end
   #BEGIN public methods that should be your starting point
 
   def move_to!(x, y, z)
+    raise 'Cuboid already at this origin' if @origin == [x,y,z]
+    @origin = [x,y,z]
   end
 
   def vertices
-    bbl = @origin #back-bottom-left
-    bbr = [x + @length, y, z] #back-bottom-right
-    fbl = [x, y, z + @width] #front-bottom-left
-    fbr = [x + @length, y, z + @width] #front-bottom-right
-    
+    {
+      :bbl => @origin, #back-bottom-left
+      :bbr => [@origin[0] + @width, @origin[1], @origin[2]], #back-bottom-right
+      :fbl => [@origin[0], @origin[1], @origin[2] + @leng], #front-bottom-left
+      :fbr => [@origin[0] + @width, @origin[1], @origin[2] + @leng], #front-bottom-right
+      :btl => [@origin[0], @origin[1] + @height, @origin[2]], #back-top-left
+      :btr => [@origin[0] + @width, @origin[1] + @height, @origin[2]], #back-top-right
+      :ftl => [@origin[0], @origin[1] + @height, @origin[2] + @leng], #front-top-left
+      :ftr => [@origin[0] + @width, @origin[1] + @height, @origin[2] + @leng] #front-top-right
+    }
   end
 
   #returns true if the two cuboids intersect each other.  False otherwise.
   def intersects?(other)
+    x_axis_start = [self.vertices[:bbl][0], other.vertices[:bbl][0]].max
+    x_axis_end = [self.vertices[:bbr][0], other.vertices[:bbr][0]].min
+
+    y_axis_start = [self.vertices[:bbl][1], other.vertices[:bbl][1]].max
+    y_axis_end = [self.vertices[:btl][1], other.vertices[:btl][1]].min
+
+    z_axis_start = [self.vertices[:bbl][2], other.vertices[:bbl][2]].max
+    z_axis_end = [self.vertices[:fbl][2], other.vertices[:fbl][2]].min
+
+    #If any of the conditions below return true, there is an overlap on that axis.
+    #Assuming that if one or two axis return true, the cuboids are stacked next to each other on those axis.
+    #If all three conditions return true, there is an intersection between the two cuboids.
+    (x_axis_end > x_axis_start) && (y_axis_end > y_axis_start) && (y_axis_end > y_axis_start)
   end
+
 
   #END public methods that should be your starting point
 end
+
+# cube = Cuboid.new([0,0,0],1,1,1)
+# cube2 = Cuboid.new([3,4,5],1,1,1)
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,17 +1,64 @@
 require 'cuboid'
 
-#This test is incomplete and, in fact, won't even run without errors.  
+#This test is incomplete and, in fact, won't even run without errors.
 #  Do whatever you need to do to make it work and please add your own test cases for as many
 #  methods as you feel need coverage
 describe Cuboid do
- 
-  describe "move_to" do
-    it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
+  subject(:cube) {Cuboid.new([0,0,0],1,2,3)}
+  let(:cube2) {Cuboid.new([2,2,2],3,4,5)}
+  let(:cube3) {Cuboid.new([0,1,1],2,4,6)}
+
+  describe "#initialize" do
+    it "sets origin to [0,0,0]" do
+      expect(cube.origin).to eq([0,0,0])
     end
-  end    
-  
+
+    it "sets cuboid's width to 1" do
+      expect(cube.width).to eq(1)
+    end
+
+    it "sets cuboid's height to 2" do
+      expect(cube.height).to eq(2)
+    end
+
+    it "sets cuboid's length to 3" do
+      expect(cube.leng).to eq(3)
+    end
+
+  end
+
+  describe "#move_to!" do
+    it "changes the origin in the simple happy case" do
+      expect(cube.move_to!(1,2,3)).to eq(cube.origin)
+    end
+
+    it "does not allow moving to the same origin" do
+      cube.move_to!(1,2,3)
+      expect do
+        cube.move_to!(1,2,3)
+      end.to raise_error("Cuboid already at this origin")
+    end
+  end
+
+  describe "#vertices" do
+    it "gets the vertices of the cuboid" do
+      expect(cube.vertices).to eq({:bbl=>[0, 0, 0],
+                                   :bbr=>[1, 0, 0],
+                                   :fbl=>[0, 0, 3],
+                                   :fbr=>[1, 0, 3],
+                                   :btl=>[0, 2, 0],
+                                   :btr=>[1, 2, 0],
+                                   :ftl=>[0, 2, 3],
+                                   :ftr=>[1, 2, 3]})
+    end
+  end
+
   describe "intersects?" do
+    it "checks if two cubes overlap/intersect each other" do
+      expect(cube.intersects?(cube2)).to be false
+      expect(cube.intersects?(cube3)).to be true
+    end
+
   end
 
 end
